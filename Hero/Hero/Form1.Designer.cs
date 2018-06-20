85386ab572e8fa71a63667e9b935053b155076cb
Jurgis1997 <36620681+Jurgis1997@users.noreply.github.com>
2018-06-20 12:30:08 +0300

    Add files via upload

---
+++ Hero/Hero/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Hero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(96, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 560);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(781, 439);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "index.png");
+            this.imageList1.Images.SetKeyName(1, "1797763_1.jpg");
+            this.imageList1.Images.SetKeyName(2, "avocado-sliced.jpg");
+            this.imageList1.Images.SetKeyName(3, "images.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 566);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Hero_Keydown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Hero/Hero/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hero
+{
+    public partial class Form1 : Form
+    {
+        Random random;
+        enum Board
+        {
+            Space,
+            Hero,
+            Fruit
+        };
+        enum Movement
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        };
+        struct HeroCoord
+        {
+            public int x;
+            public int y;
+        }
+
+        Board[,] gameboard;
+        HeroCoord[] HeroXY;
+        int Herosize;
+        Movement move;
+        Graphics D;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameboard = new Board[11, 11];
+            HeroXY = new HeroCoord[100];
+            random = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(420, 420);
+            D = Graphics.FromImage(pictureBox1.Image);
+            D.Clear(Color.White);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                D.DrawImage(imageList1.Images[3], i * 35, 0);
+                D.DrawImage(imageList1.Images[3], i * 35, 385);
+            }
+            for (int i = 0; i <= 11; i++)
+            {
+                D.DrawImage(imageList1.Images[3], 0, i * 35);
+                D.DrawImage(imageList1.Images[3], 385, i * 35);
+            }
+
+            HeroXY[0].x = 5;
+            HeroXY[0].y = 5;
+            HeroXY[1].x = 5;
+            HeroXY[1].y = 6;
+            HeroXY[2].x = 5;
+            HeroXY[2].y = 7;
+
+            D.DrawImage(imageList1.Images[1], 5 * 35, 5 * 35);
+            D.DrawImage(imageList1.Images[0], 5 * 35, 6 * 35);
+            D.DrawImage(imageList1.Images[0], 5 * 35, 7 * 35);
+
+            gameboard[5, 5] = Board.Hero;
+            gameboard[5, 6] = Board.Hero;
+            gameboard[5, 7] = Board.Hero;
+
+            move = Movement.Up;
+            Herosize = 3;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Fruit();
+            }
+        }
+        private void Fruit()
+        {
+            int x, y;
+            do
+            {
+            x = random.Next(1, 10);
+            y = random.Next(1, 10);
+            }
+            while (gameboard[x, y] != Board.Space);
+
+            gameboard[x, y] = Board.Fruit;
+            D.DrawImage(imageList1.Images[2], x * 35, y * 35);
+
+        
+        }
+
+        private void Hero_Keydown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Up:
+                    move = Movement.Up;
+                    break;
+                case Keys.Down:
+                    move = Movement.Down;
+                    break;
+                case Keys.Left:
+                    move = Movement.Left;
+                    break;
+                case Keys.Right:
+                    move = Movement.Right;
+                    break;
+            }
+        }
+        private void GameOver()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("Better luck next time.");
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            D.FillRectangle(Brushes.White, HeroXY[Herosize - 1].x * 35, HeroXY[Herosize - 1].y * 35,35,35);
+            gameboard[HeroXY[Herosize - 1].x, HeroXY[Herosize - 1].y] = Board.Space;
+            pictureBox1.Refresh();
+
+            for ( int i = Herosize; i >= 1; i--)
+            {
+                HeroXY[i].x = HeroXY[i - 1].x;
+                HeroXY[i].y = HeroXY[i - 1].y;
+            }
+            D.DrawImage(imageList1.Images[0], HeroXY[0].x * 35, HeroXY[0].y * 35);
+
+            switch (move)
+            {
+                case Movement.Up:
+                    HeroXY[0].y = HeroXY[0].y - 1;
+                    break;
+                case Movement.Down:
+                    HeroXY[0].y = HeroXY[0].y + 1;
+                    break;
+                case Movement.Left:
+                    HeroXY[0].x = HeroXY[0].x - 1;
+                    break;
+                case Movement.Right:
+                    HeroXY[0].x = HeroXY[0].x + 1;
+                    break;
+            }
+
+            if (HeroXY[0].x < 1 || HeroXY[0].x > 10 || HeroXY[0].y < 1 || HeroXY[0].x > 10)
+            {
+                GameOver();
+                pictureBox1.Refresh();
+                return;
+            }
+
+            if (gameboard[HeroXY[0].x,HeroXY[0].y] == Board.Hero)
+            {
+                GameOver();
+                pictureBox1.Refresh();
+                return;
+            }
+            if (gameboard[HeroXY[0].x, HeroXY[0].y] == Board.Fruit)
+            {
+                D.DrawImage(imageList1.Images[0], HeroXY[Herosize].x * 35, HeroXY[Herosize].y * 35);
+                gameboard[HeroXY[Herosize].x, HeroXY[Herosize].y] = Board.Hero;
+                Herosize++;
+
+                if (Herosize<99)
+                {
+                    Fruit();
+                }
+                this.Text = "Hero - score:" + Herosize;
+            }
+            D.DrawImage(imageList1.Images[1], HeroXY[0].x * 35, HeroXY[0].y * 35);
+            gameboard[HeroXY[0].x, HeroXY[0].y] = Board.Hero;
+            pictureBox1.Refresh();
+        }
+    }
+}
